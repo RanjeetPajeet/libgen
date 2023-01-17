2b89516ef45e63f658384eb204014d9d63ee58b1
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-16 18:49:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,16 @@ st.set_page_config(
 
 st.title("Libgen library")
 st.markdown("---")
+
+
+
+with st.container():
+    st.markdown("## Search")
+    st.markdown("###")
+    
+    title_col, author_col = st.columns(2)
+    with title_col:
+        title = st.text_input("Book Title", "")
+    with author_col:
+        author = st.text_input("Author Name", "")
+        
