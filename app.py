1e52d9850afbc092327667531dc73f622c306e40
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-18 12:23:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -37,3 +37,9 @@ with st.container():
 submit_button = st.button("Submit")
 if submit_button:
     st.write(search.by_title(title))
+
+    
+submit_button2 = st.button("Submit2")
+if submit_button2:
+    st.write(search.by_title_and_author(title,author))
+    
