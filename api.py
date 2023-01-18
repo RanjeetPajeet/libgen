40940589af9b6518922b471eda1b47f34bc90dd4
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-18 12:16:34 -0600

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,43 @@ api.py
 Summary of `libgen-api` methods.
 """
 
-import libgen_api as libgen
+import libgen_api
 
 
 
+search = Search()
+
+
+
+class Search:
+  def __init__(self):
+    self.filters = {
+      "Extension": "pdf",
+    }
+    
+  
+  
+#   @staticmethod
+  def by_title(self, title: str):
+    s = libgen_api.LibgenSearch()
+    results = s.search_title(title)
+    return results
+  
+  
+#   @staticmethod
+  def by_author(self, author: str):
+    s = libgen_api.LibgenSearch()
+    results = s.search_author(author)
+    return results
+  
+  
+#   @staticmethod
+  def by_title_and_author(self, title: str, author: str):
+    s = libgen_api.LibgenSearch()
+    results = s.search_title_filtered( title, {"Extension":"pdf", "Author":author}, exact_match=True )
+    return results
+  
+  
+#   @staticmethod
+  def get_downloads(self, search_results: dict):
+    pass
